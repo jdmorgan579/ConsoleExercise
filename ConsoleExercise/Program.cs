bdf561070f4b7eb8925fd983f19d2dbbe99fcd3e
jdmorgan579 <jordan.morgan@carvana.com>
2017-11-15 14:13:13 -0700

    Solution
    solution

--- ConsoleExercise/Program.cs
+++ ConsoleExercise/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleExercise
@@ -10,6 +11,18 @@ namespace ConsoleExercise
     {
         static void Main(string[] args)
         {
+            string randomString = "kd$2 23re034()9";
+
+            string cleanString = Regex.Replace(randomString, "[^A-Za-z0-9 ]", "");
+
+            string cleanestString = Regex.Replace(cleanString, " ", "");
+
+            string reverseString = new string(cleanestString.ToCharArray().Reverse().ToArray());
+
+            Console.WriteLine(reverseString);
+            Console.ReadLine();
+
         }
     }
 }
+'\n'
